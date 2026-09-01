eddd7be6168d8b22f4c52c86d372211df381af2e
krishnanandiraju <krishnanandiraju@gmail.com>
2026-09-01 21:11:51 +0530

    style: load fit memory polish

--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -2,6 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import './features/demo/integration-polish.css'
+import './features/demo/fit-memory-polish.css'
 import ClientDemoWorkspace from './features/demo/ClientDemoWorkspace'
 
 createRoot(document.getElementById('root')!).render(
